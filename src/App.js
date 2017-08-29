c14e91286df18a9789740f20b97f0b6d44e60564
Matt Wigdahl <mlwigdahl@gmail.com>
2017-08-28 19:38:54 -0500

    favicon and jumbotron

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+import RFSJumbo from './main/RFSJumbo.js';
 import './App.css';
+import logo from './logo.svg';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => 
+  <div className="App">
+    <RFSJumbo />
+  </div>
 
 export default App;
+
+
+/*       <div class="builtwith">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div id="content">
+          <h3>Built with React</h3>
+        </div>
+      </div> */
---
+++ src/main/RFSJumbo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+const RFSJumbo = () =>
+    <Jumbotron>
+        <h1>Radio Free Serverless</h1>
+        <p />
+    </Jumbotron>
+
+export default RFSJumbo;
